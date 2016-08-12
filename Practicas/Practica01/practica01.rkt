104ae859c076ffaf1d1c3848152d7c1b09594ff3
ivan <ivan@amarello.com.mx>
2016-08-11 19:35:28 -0500

    practica 1 Lenguajes

---
+++ Practicas/Practica01/practica01.rkt
@@ -0,0 +1,32 @@
+#lang plai
+
+;1) Ecuación de primer grado
+(define (ec-lin a b)
+  (/ (* -1 b) a))
+
+;2) Fórmula de Herón
+;Funcion S
+(define (S a b c)
+  (/ (+ a (+ b c)) 2))
+
+(define (area-heron a b c)
+  (sqrt (* (S a b c) (* (- (S a b c) a) (* (- (S a b c) b) (- (S a b c) c))))))
+
+;3) Triangulos rectangulos
+(define (triangulo-rec? a b c)
+  (if(= (+ (* a a) (* b b)) (* c c))
+     #t
+     #f))
+
+;4)s-digitos
+(define (s-digitos n)
+  (if (= n 0)
+      0 
+      (+ (modulo n 10) 
+         (s-digitos (/ (- n (modulo n 10)) 10)))))
+
+;5)Inversión de números
+(define (invierte n)
+  (if (< n 10)
+      n
+      (+ ())))
